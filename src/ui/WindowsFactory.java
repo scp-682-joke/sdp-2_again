5d4035827da4a340cbd71c8059b92059377c03f1
scp-682-joke <zhibek_slambekova@mail.ru>
2026-09-20 18:21:10 +0500

    all done

---
+++ src/ui/WindowsFactory.java
@@ -0,0 +1,13 @@
+package ui;
+
+public class WindowsFactory implements GUIFactory {
+    @Override
+    public Button createButton() {
+        return new WindowsButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new WindowsCheckbox();
+    }
+}
